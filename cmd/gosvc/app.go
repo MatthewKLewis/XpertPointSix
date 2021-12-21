7c144b9bdb7293813e5752b7a34c917bfbb889f6
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-12-21 09:29:19 -0500

    ready for packets

--- cmd/gosvc/app.go
+++ cmd/gosvc/app.go
@@ -1,13 +1,40 @@
 package main
 
+import (
+	"net"
+)
+
+const (
+	UDP_LISTEN_CONNECTION = "192.168.3.157" // Matthew's Laptop
+)
+
 // The wrapper of your app
 func yourApp(s server) {
-
-	// This is just some sample code to do something
 	s.winlog.Info(1, "In Xpert PointSix Parse")
-	s.winlog.Info(1, "Still parsing...")
-	s.winlog.Info(1, "And parsing...")
-	s.winlog.Info(1, "And the service will keep parsing...")
+
+	packet := make([]byte, 65536)
+	addr := net.UDPAddr{
+		Port: 8552,
+		IP:   net.ParseIP(UDP_LISTEN_CONNECTION), //Ethernet on Go Dev Server Side?
+	}
+	s.winlog.Info(1, "Address set to: "+addr.IP.String())
+
+	server, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		s.winlog.Info(1, "Error on server set up: "+err.Error())
+		panic(err)
+	}
+
+	s.winlog.Info(1, "Server string: "+server.LocalAddr().Network())
+
+	for {
+		//Read packets on the UDP connection
+		_, remoteaddr, err := server.ReadFromUDP(packet) //is this 'blocking' waiting for a UDP message to come in?
+		if err != nil {
+			s.winlog.Info(1, "Error on UDP read: "+err.Error()+remoteaddr.Network())
+		}
+		s.winlog.Info(1, string(packet))
+	}
 
 	// Notice that if this exits, the service continues to run - you can launch web servers, etc.
 }
